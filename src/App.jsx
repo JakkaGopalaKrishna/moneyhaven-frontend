5d34a8ceb406d21fa87731c253cc70612a665939
JakkaGopalaKrishna <jakkakrishna2003@gmail.com>
2026-06-24 13:51:59 +0530

    feat: implement authentication loading states

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
 import { ConfigProvider, theme } from 'antd';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect, useState } from 'react';
 import AppRoutes from './routes/AppRoutes';
 import useTheme from './hooks/useTheme';
+import LoadingPage from './pages/Loading/LoadingPage';
+import { getCurrentUser } from './store/authSlice';
 
 function App() {
   const { isDarkMode } = useTheme();
+  const { token, user } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  const [isInitializing, setIsInitializing] = useState(true);
+
+  useEffect(() => {
+    const initializeAuth = async () => {
+      if (token && !user) {
+        await dispatch(getCurrentUser()).unwrap().catch(() => {
+          // Token invalid or expired, authSlice handles clearing state
+        });
+      }
+      setIsInitializing(false);
+    };
+
+    initializeAuth();
+  }, [dispatch, token, user]);
+
+  if (isInitializing) {
+    return <LoadingPage />;
+  }
 
   return (
     <ConfigProvider
