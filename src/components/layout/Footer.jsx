0e40a4e90932378a483d5f25b2af4bf7a233742b
JakkaGopalaKrishna <jakkakrishna2003@gmail.com>
2026-06-25 10:14:37 +0530

    feat: enhance dashboard footer content and layout

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -2,8 +2,11 @@ import { Outlet } from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <footer className="bg-white dark:bg-[#1f1f1f] text-gray-500 dark:text-gray-400 py-4 text-center text-sm border-t border-gray-200 dark:border-gray-800 transition-colors duration-200">
-      <p>&copy; {new Date().getFullYear()} MoneyHaven. All rights reserved.</p>
+    <footer className="bg-white dark:bg-[#1f1f1f] text-gray-500 dark:text-white py-4 text-center text-sm border-t border-gray-200 dark:border-gray-800 transition-colors duration-200">
+      <div className="mx-4 mx-auto text-center md:text-left text-sm flex flex-col md:flex-row justify-between items-center">
+        <p>© {new Date().getFullYear()} MoneyHaven. All rights reserved.</p>
+        <p className="md:mt-0">Built with 💙 for personal finance.</p>
+      </div>
     </footer>
   );
 };
